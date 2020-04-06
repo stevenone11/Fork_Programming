610b38658a48063f55ad0f9933a9a7c20046d58d
stevenone11 <stevencenteno13@gmail.com>
2020-04-05 19:45:09 -0700

    Added kill command

--- Fork_Programming.cpp
+++ Fork_Programming.cpp
@@ -10,52 +10,72 @@
 #include <random>
 #include <algorithm>
 #include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "kill_patch.h"
 using namespace std;
 
 //This program is supposed to:
 //
 //	I). print 2 options to choose from 1. Run Program, 2. Exit Program
+//
 // II). ask the user for a letter to search for in the size of the array.
+//
 //III). ask the user for the size of the array
+//
 // IV). spawns child process (after asking for the size fo the new array), 
 //		to fill the array with raw bytes of upper Random capital letters
+//
 //  V). child process searches the array for the inputted letter and output 
 //		the number of occurrances of said letter. Each child should only 
 //		read 1 letter and terminate after reading said letter. THen after outputting
 //		the total number of letters terminate the whole program.
+//
 // VI). when the software (child process) cannot read any number of occurrences, 
 //		it restarts the search in the array and as a result, needs another terminal to
 //		enter a kill command to kill the child
+//
 
 
 //initialize methods for the program
 bool menuSelection();
 void createChild();
-vector<char> createRandArr(int arrSize);
+char* createRandArr(int arrSize);
 
 //global variables
-string searchChar;
+char searchChar;
 int arrSize;
+int len;
+
+
+//the buffer that is used for the force terminate
+struct buf {
+    long mtype;
+    char greeting[50]; 
+};
+
+//initializing the buffer
+buf msg;
 
 
 /*
 * Creates a random capital character bewteen A and Z and returns it
 **/
-vector<char> createRandArr(int arrSize) {
+char* createRandArr(int arrSize) {
 	
 	int randNum;
-	String randChar;
-	vector<char> vec;
+	char randChar;
+	char *array = new char[arrSize];
 
 	//iterates up to the size of the array and 
 	for (int Itr = 0; Itr < arrSize; Itr++)
 	{
 		randNum = rand() % 26;
 		randChar = 'A' + randNum;
-		vec.push_back(randChar);
+		array[Itr] = randChar;
 	}
 
-	return vec;
+	return array;
 };
 
 
@@ -98,23 +118,31 @@ void createChild(){
 
 		
 		//creates the array using the inputs given earlier by the user and saves it into a variable
-		vector<char> userArr = createRandArr(arrSize);
+		char* userArr = createRandArr(arrSize);
 		cout << "the array is: " << userArr << endl;
 
 		//now looks through the array to get the letter the user wants
 		//if it does not exist, will wait for a kill command to end the process of the child
 
 		//kill porcess command should be here
-
+		//uses the kill patch to end the process if it is manually cut off from
+		//another terminal
+		/**
+		buf exitMsg;
+    	len = sizeof(msg)-sizeof(long);
+    	exitMsg.mtype = 1;
+    	strcpy(exitMsg.greeting, "C_Leaves");
+    	kill_patch(qid, (struct msgbuf *)&exitMsg, len, 1);
+		**/
 
 		while(*PointerLetterCount < 1) {
 			
 			//loops through the array, searching for a character
-			for(auto i: userArr) {
+			for(int i = 0; i < arrSize; i++) {
 
 				//if the child finds a character, it will create a new child to find the next character
 				//with this the pointer will add to the num 
-				if(userArr[i] == searchChar) {
+				if(char(userArr[i]) == searchChar) {
 
 					//changes the count of letters by incrementing it by one, since its an address in memory
 					//despite being different processes, it should add to the total num of letters
@@ -199,7 +227,9 @@ int main() {
 	bool userHasntExit = true;
 	while (userHasntExit) {
 		
-		if(menuSelection()) { 
+		bool TerminateTheProcess = menuSelection();
+
+		if(TerminateTheProcess) { 
 			userHasntExit = false;
 		}
 
---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ Fork_Programming.cpp kill_patch64.o -o a.out
